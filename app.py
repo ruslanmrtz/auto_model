91f048a2d32bd0b33b55743ccc0674d2cdbe526e
ruslanmrtzn <ruslanmrtzn@yandex.ru>
2024-04-24 20:55:10 +0500

    api development

--- app.py
+++ app.py
@@ -5,12 +5,25 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('predict.html')
+    return render_template('index.html')
 
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    return 
+    result = dict(request.form)
+    print(result)
+    if 'No' in result['brand']:
+        return render_template('error.html', attr='марку автомобиля')
+    elif 'No' in result['fuel_type']:
+        return render_template('error.html', attr='тип топлива')
+    elif 'No' in result['engine']:
+        return render_template('error.html', attr='объем двигателя')
+    elif 'No' in result['color']:
+        return render_template('error.html', attr='цвет кузова')
+    elif 'accident' not in result.keys():
+        return render_template('error.html', attr='информацию о авариях')
+
+    return render_template('predict.html', cost=2)
 
 
 
